0447ded4ce2c072f07ed6bbacda26f25c72d5260
Bariatti Arnaud <Arnaud.Bariatti@swisscom.com>
2018-04-26 13:22:25 +0200

    initial commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+import { HomeComponent } from './pages/home/home.component';
+import { AboutComponent } from './pages/about/about.component';
+import { ProfileComponent } from './pages/profile/profile.component';
+import { LoginComponent } from './pages/login/login.component';
+import { ProductComponent } from './pages/product/product.component';
+import { AuthGuard } from './guards/auth.guard';
+
+const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'login', component: LoginComponent },
+  { path: 'home', component: HomeComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'profile', component: ProfileComponent, canActivate: [AuthGuard]  },
+  { path: 'products', component: ProductComponent, canActivate: [AuthGuard]  },
+];
+
+@NgModule({
+  imports: [ RouterModule.forRoot(routes)],
+  exports: [ RouterModule ]
+})
+export class AppRoutingModule { }
---
+++ src/app/app.module.ts
@@ -0,0 +1,73 @@
+// import { JwtInterceptor } from './helpers/jwt.interceptor';
+import { ParseInterceptor } from './helpers/parse.interceptor';
+import { ProductService } from './services/product.service';
+import { LoginComponent } from './pages/login/login.component';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { AppRoutingModule } from './app-routing.module';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AppMaterialModule } from './shared/app-material/app-material.module';
+import { ThemePickerModule } from './shared/theme-picker';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { ProfileComponent } from './pages/profile/profile.component';
+import { AboutComponent } from './pages/about/about.component';
+import { environment } from '../environments/environment';
+import { HttpClient, HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { AlertService } from './services/alert.service';
+import { AuthGuard } from './guards/auth.guard';
+import { AuthenticationService } from './services/authentication.service';
+import { ProductComponent } from './pages/product/product.component';
+
+// AoT requires an exported function for factories
+export function HttpLoaderFactory(http: HttpClient) {
+  return new TranslateHttpLoader(http, './assets/i18n/', '.json');
+}
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    HomeComponent,
+    ProfileComponent,
+    AboutComponent,
+    ProductComponent,
+  ],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: HttpLoaderFactory,
+        deps: [HttpClient]
+      }
+    }),
+    ServiceWorkerModule.register('/ngsw-worker.js', { enabled: environment.production }),
+    FormsModule,
+    ReactiveFormsModule,
+    AppRoutingModule,
+    AppMaterialModule,
+    FlexLayoutModule,
+    BrowserAnimationsModule,
+    ThemePickerModule
+  ],
+  providers: [
+    AuthGuard,
+    AlertService,
+    AuthenticationService,
+    ProductService,
+    {
+        provide: HTTP_INTERCEPTORS,
+        useClass: ParseInterceptor,
+        multi: true
+    },
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
